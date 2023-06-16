650deb57960cb0d2507ae534a15f3d3d88c38a59
Jonathan Im <imjonathan.work@gmail.com>
2023-06-15 19:59:28 -0700

    working on Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import respicture from './assets/restauranfood.jpg'
+import './Header.css'
 const Header = () => {
   return (
     <header>
+      <div className='header-left-container'>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>We are a family owned, Mediterranean restaurant,<br/> focused on traditional <br/>recipes served with a modern <br/> twist.</p>
+        <button className='header-reserve-button'>Reserve a table</button>
+      </div>
+      <div className='header-right-container'>
+        <img src={respicture} alt='Picture of server carrying a dish.' className='header-restaurant-picture'/>
+      </div>
       
     </header>
   )
