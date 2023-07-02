b43ead83df38db650e86646d3e6855e33ddaa95e
Jonathan Im <imjonathan.work@gmail.com>
2023-07-02 10:27:17 -0700

    working on about page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './styles/About.css'
+
 
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
+
+    <div className='about-container'>
+          <div className='about-wrapper'>
+       
+      <div className="about-center-container">
+        <div className='about-text'>
+        <h2 >Little Lemon Restaurant</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet possimus velit reprehenderit cumque fugit sunt deleniti officiis aperiam soluta atque, facere dolore perferendis molestias veniam eligendi odit culpa deserunt tempore voluptatum provident reiciendis optio repellendus! Praesentium quam ratione rerum rem hic doloremque. Maiores?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti, blanditiis. Quaerat pariatur accusantium similique laboriosam provident ratione minus autem natus?</p>
+        </div>
+      </div>
+     </div>
     </div>
   )
 }
